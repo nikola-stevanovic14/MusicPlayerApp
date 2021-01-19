a9d9255a566cb800b40ef6f53e0d59455af65b7f
nikola-stevanovic14 <nikolastevanovic815@gmail.com>
2021-01-19 01:18:12 +0100

    app icon and title added, main process debugging conf set

--- src/main.js
+++ src/main.js
@@ -2,26 +2,32 @@
 const projectRootFolder = '../';
 const path = require('path');
 
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, BrowserView } = require('electron');
 const config = require(path.join(projectRootFolder, 'configuration/config.json'));
 const windowsConfig = require(path.join(projectRootFolder, 'configuration/windowsConfig.json'));
 const windowsApiRouter = require(path.join(projectRootFolder, config.windowsApi.windowsApiRouterLocation));
 
 let windows = new Map();
 
-
+app.setName('Music Player');
 
 function createWindow (options) {
+  options.properties.icon = path.join(config.images.imagesFileLocation, options.iconPath);
   const win = new BrowserWindow(options.properties);
   win.loadFile(path.join(config.views.viewsFolderLocation, options.windowViewFile));
   windows[options.key] = windowsApiRouter.getApi(options, win);
-  if (options.openDevTools)
+  if (options.openDevTools){
     win.webContents.openDevTools();
+  }
+  view = new BrowserView()
+  view.webContents.loadURL(path.join(config.views.viewsFolderLocation, 'view.html'));
+  win.setBrowserView(view)
+  view.setBounds({ x: 150, y: 150, width: 300, height: 300 })
 }
 
 
-app.whenReady().then(function(){
-  return createWindow(windowsConfig.mainWindow)
+app.whenReady().then(()=>{
+  return createWindow(windowsConfig.mainWindow);
 });
 
 
@@ -36,3 +42,4 @@ app.on('activate', () => {
     createWindow(windowsConfig.mainWindow);
   }
 });
+
